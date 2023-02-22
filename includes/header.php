209e0f3d847c108dc18e37dbaa14b31b99aee02c
Dn1San <c0009613@hallam.shu.ac.uk>
2023-02-22 00:17:00 +0000

    Start

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<header>
+    <div id="logo">
+        
+    </div>
+    <nav>
+        <ul>
+            <li><a href="profile.php">Profile</a></li>
+            <li><a href="friendsList.php">Friends</a></li>
+            <li><a href="search.php">Search</a></li>
+            <li><a href="create.php">Create</a></li>
+            <li><a href="notifications.php">Notifications</a></li>
+        </ul>
+        <a href="login.php">Sign Out</a>
+    </nav>
+</header>
