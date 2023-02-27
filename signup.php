ab27b2355430374850d49e97c69ae69e7c74f264
Dn1San <c0009613@hallam.shu.ac.uk>
2023-02-27 23:34:52 +0000

    Login and Register Touchup 3

--- signup.php
+++ signup.php
@@ -8,18 +8,25 @@
     <link rel="stylesheet" type="text/css" href="CSS/main.css"/>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter"/>
     <style>
-        #logo h1{
+        .logo h1{
             font-family: Inter;
             font-weight: 700;
             font-size: 60px;
             font-style: italic;
         }
+        .logo h2{
+        font-family: Inter;
+        font-weight: 700;
+        font-size: 27px;
+        font-style: italic;
+      }
     </style>
     <script src="js/main.js"></script>
 </head>
 <body>
-    <div id=logo>
+    <div class="logo center">
         <h1>NAME<br>MEDIA</h1>
+        <h2>We work to connect and share.</h2>
     </div>
     <div class="registerbox">
         <h2>SIGN UP</h2>
